b041f6c843ce02020338a896f6283decd022c574
ryuichi-oka <rrrr419rrrr@gmail.com>
2022-04-13 12:22:23 +0000

    トップページ作成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "fdbd7c723a0e46fbbb523a8426fe5b1c.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  root to: "homes#top"
+  get "/about" => "homes#about", as: "about"
+
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+
+  devise_for :members, skip: [:passwords], controllers: {
+    registrations: "member/registrations",
+    sessions: "member/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
