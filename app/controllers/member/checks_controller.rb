0fdb8676680736454bf8733a904dc0737cfbf605
ryuichi-oka <rrrr419rrrr@gmail.com>
2022-05-20 02:33:00 +0000

    既読機能非同期通信化

--- app/controllers/member/checks_controller.rb
+++ app/controllers/member/checks_controller.rb
@@ -2,16 +2,18 @@ class Member::ChecksController < ApplicationController
   before_action :authenticate_member!
 
   def create
-    group_message = GroupMessage.find(params[:group_message_id])
-    check = current_member.checks.new(group_message_id: group_message.id)
+    @group_message = GroupMessage.find(params[:group_message_id])
+    @members = @group_message.group.members.where(is_active: true)
+    check = current_member.checks.new(group_message_id: @group_message.id)
     check.save
-    redirect_to member_group_message_path(group_message.id)
+    # redirect_to member_group_message_path(group_message.id)
   end
 
   def destroy
-    group_message = GroupMessage.find(params[:group_message_id])
-    check = current_member.checks.find_by(group_message_id: group_message.id)
+    @group_message = GroupMessage.find(params[:group_message_id])
+    @members = @group_message.group.members.where(is_active: true)
+    check = current_member.checks.find_by(group_message_id: @group_message.id)
     check.destroy
-    redirect_to member_group_message_path(group_message.id)
+    # redirect_to member_group_message_path(group_message.id)
   end
 end
